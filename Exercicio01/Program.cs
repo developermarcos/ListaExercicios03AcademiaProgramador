f530ab62d68ce3de9bfe26b17b54f999e646cf25
Marcos Lima <developermarcos@outlook.com>
2022-02-08 14:18:53 -0300

    Add project files.

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
